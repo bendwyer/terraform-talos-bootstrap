f9e862c6aada8ebdc840c30f622a2fe5a781e850
bendwyer <17102207+bendwyer@users.noreply.github.com>
2026-01-04 13:23:46 +0000

    feat: Parameterize config file paths and allow file generation to be skipped

--- variables.tf
+++ variables.tf
@@ -1,3 +1,10 @@
+variable "kubeconfig_path" {
+  description = "Path where the kubeconfig file will be saved. Supports tilde expansion. Set to null to skip kubeconfig file creation."
+  type        = string
+  default     = "~/.kube/config"
+  nullable    = true
+}
+
 variable "talos_cluster_endpoint" {
   description = "Hostname or IP address for the Talos cluster endpoint (without https:// or port). Used as the Kubernetes API endpoint address."
   type        = string
@@ -63,4 +70,11 @@ variable "talos_vip" {
 variable "talos_worker_config_patches" {
   description = "List of machine configuration patch file paths for worker nodes. Files ending in .tftpl are processed as templates with access to the following variables: hostname, install_disk, node_ip, cluster_name, cluster_endpoint. Paths are relative to the calling module."
   type        = list(string)
+}
+
+variable "talosconfig_path" {
+  description = "Path where the talosconfig file will be saved. Supports tilde expansion. Set to null to skip talosconfig file creation."
+  type        = string
+  default     = "~/.talos/config"
+  nullable    = true
 }
