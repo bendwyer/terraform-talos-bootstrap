b75311c22dc60dfbe902964275a2e40fcbef6931
bendwyer <17102207+bendwyer@users.noreply.github.com>
2026-01-06 22:27:46 +0000

    style: Fix missing spaces

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">=1.12.2"
+  required_version = ">= 1.12.2"
 
   required_providers {
     local = {
       source  = "hashicorp/local"
-      version = ">=2.5.3"
+      version = ">= 2.5.3"
     }
     talos = {
       source  = "siderolabs/talos"
